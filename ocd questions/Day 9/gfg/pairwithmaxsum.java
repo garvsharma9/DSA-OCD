7904b078bdf4e8fdae5ab743f945f2b6e2f5a05a
garv's repo <garvroc9@gmail.com>
2025-03-10 17:10:27 +0530

    Q3 code change

--- ocd questions/Day 9/gfg/pairwithmaxsum.java
+++ ocd questions/Day 9/gfg/pairwithmaxsum.java
@@ -1,21 +1,11 @@
-class pairwithmaxsum {
-    public:
-      int pairWithMaxSum(vector<int> &arr) {
-         int  n=arr.size();
-            int msum=-1;
-      for (int i=0;i<n-1;i++){
-          int mini=min(arr[i],arr[i+1]);//smallest 
-          int mini2=max(arr[i],arr[i+1]);//second smallest
-          msum=max(msum,mini+mini2);
-          for(int j=i+2;j<n && arr[j]<mini2;j++){
-              if(arr[j]<mini){
-                  mini2=mini;
-                  mini=arr[j];
-              }
-              int sum=mini+mini2;
-              msum=max(msum,sum);   }
-      }
-      return msum;
-      }
-  };
+public class pairwithmaxsum
+{
+    public int pairWithMaxSum(int arr[]) {
+    int maxsum = -1;
+    for(int i=0;i<arr.length-1;i++)
+    {
+       maxsum = Math.max(maxsum, arr[i]+arr[i+1]); 
+    }
+    return maxsum;
+}}
   
